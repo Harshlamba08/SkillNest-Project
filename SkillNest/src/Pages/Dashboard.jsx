a8204e8a77bace90ac964394aba4bbfad506429c
Harsh Lamba <lambaaharsh08@gmail.com>
2026-03-27 17:30:09 +0530

    final mern project with backend

---
+++ SkillNest/src/Pages/Dashboard.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const Dashboard = () => {
+
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/courses")
+      .then(res => res.json())
+      .then(data => setCourses(data));
+  }, []);
+
+  return (
+    <div className="dashboard">
+
+      <h1>User Dashboard</h1>
+      <h3>Enrolled Courses</h3>
+
+      <div className="course-list">
+        {courses.map((c) => (
+          <div key={c._id} className="card">
+            {c.title}
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Dashboard;
